13b875e703cdad92f22be742e4c2a9796e6faf4b
Matheus Benachio <matheuslopesbenachio@gmail.com>
2022-05-04 09:01:37 -0300

    feat: created tcore-plugin-filesystem-local package

---
+++ packages/tcore-plugin-filesystem-local/src/resolveFile.ts
@@ -0,0 +1,8 @@
+/**
+ * Returns the local path of the file since this is in the local disk anyway.
+ */
+async function resolveFile(filePath: string) {
+  return filePath;
+};
+
+export { resolveFile };
