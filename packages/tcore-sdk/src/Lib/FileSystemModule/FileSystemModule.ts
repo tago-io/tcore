9d7c558df29574b6f516ae94252e93623532c861
Matheus Benachio <matheuslopesbenachio@gmail.com>
2022-05-15 11:28:05 -0300

    feat: added docs and prepared sdk for version 0.4.0

--- packages/tcore-sdk/src/Lib/FileSystemModule/FileSystemModule.ts
+++ packages/tcore-sdk/src/Lib/FileSystemModule/FileSystemModule.ts
@@ -1,24 +1,25 @@
+import { IPluginFilesystemItem, IModuleSetupWithoutType } from "../../Types";
 import TCoreModule from "../TCoreModule/TCoreModule";
 
 /**
- * TODO
+ * This module allows the creation of a new filesystem.
  */
 class FileSystemModule extends TCoreModule {
-  constructor(protected setup: any) {
+  constructor(protected setup: IModuleSetupWithoutType) {
     super(setup, "filesystem");
   }
 
   /**
-   * TODO
+   * Resolves a file. Must return the path to the local file in the disk.
    */
-  public async resolveFile(path: string): Promise<any> {
-    return Promise.resolve(null);
+  public async resolveFile(path: string): Promise<string> {
+    return "";
   }
 
   /**
-   * TODO
+   * Resolves a folder structure. Must return an array of filesystem items.
    */
-  public async resolveFolder(path: string): Promise<any[]> {
+  public async resolveFolder(path: string): Promise<IPluginFilesystemItem[]> {
     return Promise.resolve([]);
   }
 }
