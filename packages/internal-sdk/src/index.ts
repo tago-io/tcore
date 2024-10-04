9d0a7e6105a775ceb3bd69eeab0263cd29b3637b
Bruno Gelatti <bgelatti@gmail.com>
2024-10-04 17:21:02 -0300

    Removed internal sdk

--- packages/internal-sdk/src/index.ts
+++
@@ -1,6 +0,0 @@
-import SidebarButtonModule from "./SidebarButtonModule/SidebarButtonModule";
-import PageModule from "./PageModule/PageModule";
-import NavbarButtonModule from "./NavbarModule/NavbarModule";
-import SystemModule from "./SystemModule/SystemModule";
-
-export { SystemModule, NavbarButtonModule, PageModule, SidebarButtonModule };
